9034c391c8b44bc9fda5c56f80b6c13ed6320440
Avadhut Vagare <avadhutvagare321@gmail.com>
2024-03-19 22:08:46 +0530

    using ternary operator and one return statement

--- src/components/ConditionalComponent.jsx
+++ src/components/ConditionalComponent.jsx
@@ -3,11 +3,5 @@ import Welcome from "./Welcome";
 
 export default function ConditionalComponent() {
   const display = true;
-  let message;
-  if (display === true) {
-    message = <h1>true element</h1>;
-  } else {
-    message = <h1>false element</h1>;
-  }
-  return message;
+  return display ? <Welcome /> : <Code />;
 }
