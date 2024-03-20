453da8693276cca972b5199ae8af4c9985a23dbb
Avadhut Vagare <avadhutvagare321@gmail.com>
2024-03-20 21:47:58 +0530

    event handling in react

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,7 +1,11 @@
 export default function Welcome() {
+  function handleClick() {
+    console.log("handle clicked");
+  }
   return (
     <div>
       <h2>Welcome component</h2>
+      <button onClick={handleClick}>Ckick Me</button>
     </div>
   );
 }
