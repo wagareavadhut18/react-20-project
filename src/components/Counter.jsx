ce6aca1a58443ed57dc147d93d924dce2d598790
Avadhut Vagare <avadhutvagare321@gmail.com>
2024-03-21 21:33:47 +0530

    creating multiple states

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,17 +2,24 @@ import { useState } from "react";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
+  const [increment_by, setIncrementBy] = useState(1);
   function handleClickIncrement() {
-    setCount(count + 1);
+    setCount(count + increment_by);
   }
   function handleClickDecrement() {
-    setCount(count - 1);
+    setCount(count - increment_by);
+  }
+  function handleClickIncrementBy() {
+    setIncrementBy(increment_by + 1);
   }
   return (
     <div>
       <h1>Count value = {count}</h1>
       <button onClick={handleClickIncrement}>Increment</button>
       <button onClick={handleClickDecrement}>Decrement</button>
+
+      <h1>We are incrementing value by : {increment_by}</h1>
+      <button onClick={handleClickIncrementBy}>Increment</button>
     </div>
   );
 }
